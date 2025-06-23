61a023907404f3de8434ff9622b29212926ccc11
JUAN19742 <jsrueda@puce.edu.ec>
2025-06-22 20:08:41 -0500

    modificacion models

--- futbol/models.py
+++ futbol/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Equipo ( models . Model ) :
-    nombre = models . CharField ( max_length =100)
-    ciudad = models . CharField ( max_length =100)
+class Equipo ( models.Model ) :
+    nombre = models.CharField( max_length =100)
+    ciudad = models.CharField( max_length =100)
 
 def __str__ ( self ) :
-    return f" { self . nombre } ␣ ({ self . ciudad }) "
+    return f" { self.nombre} ({self.ciudad}) "
 
 
